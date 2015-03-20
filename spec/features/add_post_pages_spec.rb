f332368ec1dd481bca502505869351db30c80230
Alex Kaufman <alexkaufman06@gmail.com>
2015-03-20 13:43:44 -0700

    Add integration testing for errors in the add post process

--- spec/features/add_post_pages_spec.rb
+++ spec/features/add_post_pages_spec.rb
@@ -10,4 +10,14 @@ describe "the add a post process" do
     click_on "Create Post"
     expect(page).to have_content "Post Successfully Added!"
   end
+
+  it "errors if nothing is entered by the user" do
+    visit root_path
+    click_on "Create New Post"
+    fill_in "Title", with: ""
+    fill_in "Date", with: ""
+    fill_in "Blog", with: ""
+    click_on "Create Post"
+    expect(page).to have_content "There was a problem creating your post"
+  end
 end
